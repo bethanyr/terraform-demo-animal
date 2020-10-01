cb23554e23fce4e464e1e4b7b1a4e60a1f025f7f
Bethany Rentz <bethany.rentz@hotmail.com>
2020-10-01 15:47:25 -0700

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,3 +1,17 @@
 variable "name" {
   description = "A name to pass into this module. (Demo only; unused)"
 }
+variable "docker_ports" {
+  type = list(object({
+    internal = number
+    external = number
+    protocol = string
+  }))
+  default = [
+    {
+      internal = 8300
+      external = 8300
+      protocol = "tcp"
+    }
+  ]
+}
