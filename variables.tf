7106e929cf7d7813c9c1872769900fe7253dfcf0
Bethany Rentz <bethany.rentz@hotmail.com>
2020-10-01 15:58:53 -0700

    adding more items to object

--- variables.tf
+++ variables.tf
@@ -6,12 +6,26 @@ variable "docker_ports" {
     internal = number
     external = number
     protocol = string
+    internal1 = number
+    external1 = number
+    protocol1 = string
+    internal2 = number
+    external2 = number
+    protocol2 = string
+    Internal3 = number
+    externalItem = number
+    ProtocolItem = string
+    internal10 = number
+    external10 = number
+    protocol10 = string
+    internal11 = number
+    external11 = number
+    protocol11 = string
+    internal21 = number
+    external21 = number
+    protocol21 = string
+    Internal31 = number
+    externalItem1 = number
+    ProtocolItem1 = string
   }))
-  default = [
-    {
-      internal = 8300
-      external = 8300
-      protocol = "tcp"
-    }
-  ]
 }
